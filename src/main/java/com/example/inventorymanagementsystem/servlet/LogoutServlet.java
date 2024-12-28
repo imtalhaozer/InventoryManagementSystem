57818f2886dc9954d9e01752c78cb6894409cd9b
imtalhaozer <imtalhaozer@gmail.com>
2024-12-28 20:23:19 +0300

    Feat: Added logout servlet for retailer portal

---
+++ src/main/java/com/example/inventorymanagementsystem/servlet/LogoutServlet.java
@@ -0,0 +1,27 @@
+package com.example.inventorymanagementsystem.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/log-out")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            if (request.getSession().getAttribute("retailer") != null) {
+                request.getSession().removeAttribute("retailer");
+                response.sendRedirect("login.jsp");
+            } else {
+                response.sendRedirect("index.jsp");
+            }
+
+        }
+    }
+}
